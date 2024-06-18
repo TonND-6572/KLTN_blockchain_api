7cff1349f168a5035f4ed648517d8bee34fb0549
TonND-6572 <toiiuvn111@gmail.com>
2024-06-19 02:12:43 +0700

    change check blockchain and add order tracking controlelr

---
+++ src/main/java/com/example/my_blockchain/controller/OrderTrackingController.java
@@ -0,0 +1,22 @@
+package com.example.my_blockchain.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.my_blockchain.service.OrderTrackingService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api-blockchain/order-tracking")
+public class OrderTrackingController {
+    private final OrderTrackingService orderTrackingService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> orderTracking(Long orderId) {
+        return ResponseEntity.ok(orderTrackingService.orderTracking(orderId));
+    }
+}
--- src/main/java/com/example/my_blockchain/repo/OrderTrackingRepository.java
+++ src/main/java/com/example/my_blockchain/repo/OrderTrackingRepository.java
@@ -1,5 +1,7 @@
 package com.example.my_blockchain.repo;
 
+import java.util.List;
+
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.my_blockchain.model.entity.OrderTracking;
 
 @Repository
 public interface OrderTrackingRepository extends CassandraRepository<OrderTracking, Long> {
-    
+    List<OrderTracking> findByOrderId(Long orderId);
 }
