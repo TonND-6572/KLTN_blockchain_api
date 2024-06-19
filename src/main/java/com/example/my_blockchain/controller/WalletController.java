dfb9b9cbc64a7606e3143662dac658ac13e1edb6
TonND-6572 <toiiuvn111@gmail.com>
2024-06-19 15:57:39 +0700

    modifying controller

--- src/main/java/com/example/my_blockchain/controller/WalletController.java
+++ src/main/java/com/example/my_blockchain/controller/WalletController.java
@@ -2,15 +2,26 @@ package com.example.my_blockchain.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.my_blockchain.model.mapper.WalletMapper;
+import com.example.my_blockchain.service.WalletService;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api-blockchain/wallet")
 @RequiredArgsConstructor
 public class WalletController {
+    private final WalletService walletService;
+    private final WalletMapper walletMapper;
 
+    @GetMapping("/{address}")
+    public ResponseEntity<?> getMethodName(@PathVariable String address) {
+        return ResponseEntity.ok(walletMapper.toResponse(walletService.getWallet(address)));
+    }
+    
 }
